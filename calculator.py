8f63d7cd1e85c1c5e3425032b7e25f2c7ae02541
vedantv3008 <vedant@sav.com>
2025-09-01 11:27:17 +0530

    Completed multiplication function and implemented

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 def add(a, b):
     print(f"{a} + {b} = {a + b}")
 
+def subtract(a, b):
+    print(f"{a} - {b} = {a - b}")
+
+def multiply(a, b):
+    # TODO: complete later
+    pass
+
+
 if __name__ == "__main__":
     add(2, 3)
