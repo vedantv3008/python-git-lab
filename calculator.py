2b3777403f9166e046c0b530e8835ec2ebfba1c4
vedantv3008 <vedant@sav.com>
2025-09-01 12:25:57 +0530

    changes

--- calculator.py
+++ calculator.py
@@ -1,13 +1,9 @@
 def add(a, b):
     print(f"{a} + {b} = {a + b}")
 
-def subtract(a, b):
-    print(f"{a} - {b} = {a - b}")
-
 def multiply(a, b):
     print(f"{a} * {b} = {a * b}")
 
 if __name__ == "__main__":
     add(2, 3)
-    subtract(10, 6)
     multiply(4, 5)
